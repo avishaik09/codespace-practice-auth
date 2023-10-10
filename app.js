e639279012e877b44589620ccbea26ca6b6d6e0b
Avishaik Kumar <130446596+avishaik09@users.noreply.github.com>
2023-10-10 12:13:36 +0000

    cookies and token

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ require("./database/database").connect();
 const User = require("./models/user")
 const express = require("express");
 const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const user = require('./models/user');
+const cookieParser=require('cookie-parser')
 const app = express();
 
 
 app.use(express.json());
+app.use(cookieParser());
 
 
 app.get('/', (req, res) => {
@@ -48,14 +51,56 @@ app.post("/register", async (req, res) => {
 
          }
       );
-user.token=token
+      user.token = token
+      user.password = undefined
 
+
+
+      res.status(200).json({ user, token })
+   } catch (error) {
+      console.log(error)
+   }
+})
+
+app.post("/login", async (req, res) => {
+   try {
+      //get all data from frontend
+      const { email, password } = req.body
+      //validation
+      if (!(email && password)) {
+         res.status(400).send("send all data")
+      }
+      //find user in DB
+      const user = await User.findOne({ email })
+      // !user && res.status(404).json("user not found");
+      //match the password
+      if (user && (await bcrypt.compare(password, user.password))){
+         const token = jwt.sign(
+            { id: user._id },
+            'shhhh',// process.env.jwtsecret
+            {
+               expiresIn: "2h",
+
+            }
+         );
+         user.token=token;
+         user.password=undefined;
+      }
+      //send a token in cookie
+      //cookie section
+      const option={
+         expires:new Date(Date.now()+3*24*60*60*1000),
+         httpOnly:true
+      };
+      res.status(200).cookie("token",token,option).json({
+         success:true,
+         token,
+         user
+      })
 
    } catch (error) {
       console.log(error)
    }
-
-
 })
 
 module.exports = app;
