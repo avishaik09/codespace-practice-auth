72f31c7ef189590cb51c754da0fedc6909441fd8
Avishaik Kumar <130446596+avishaik09@users.noreply.github.com>
2023-10-10 11:05:26 +0000

    mongodb connected to app

---
+++ database/database.js
@@ -0,0 +1,15 @@
+const  mongoose  = require("mongoose");
+
+const MONGODB_URL=process.env
+
+exports.connect=()=>{
+    mongoose.connect(MONGODB_URL,{useNewUrlParser:true,
+    useUnifierTopology:true})
+    .then()
+    .catch((error)=>{
+        console.log("db connection failed");
+        console.log(error)
+        process.exit(1)
+    }
+    )
+}
