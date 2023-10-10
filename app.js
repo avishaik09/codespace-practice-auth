c5744628924ae4a145b322422d2842850210ade9
Avishaik Kumar <130446596+avishaik09@users.noreply.github.com>
2023-10-10 11:23:32 +0000

    schema created

--- app.js
+++ app.js
@@ -3,10 +3,30 @@ require("./database/database").connect()
 const express=require("express");
 const app=express();
 
+
+app.use(express.json());
+
+
 app.get('/',(req,res)=>{
    res.send("<h1>Server is working</h1>");
 })
 
+app.post("/register",async(req,res)=>{
+   try {
+      //get data from body
+      //all data should exist
+      //check if user already exist
+      //encrypt the password
+      //save the user in db
+      //generate a token for user and send it
 
+      
+
+   } catch (error) {
+      console.log(error)
+   }
+
+
+})
 
 module.exports=app;
--- database/database.js
+++ database/database.js
@@ -5,7 +5,9 @@ const {MONGODB_URL}=process.env
 exports.connect=()=>{
     mongoose.connect(MONGODB_URL,{useNewUrlParser:true,
     useUnifiedTopology:true})
-    .then()
+    .then(
+        console.log("DB connected succesfully")
+    )
     .catch((error)=>{
         console.log("db connection failed");
         console.log(error)
